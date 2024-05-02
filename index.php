4680ba04e2d78d8b461a757a63375539b9a17c7f
Ovi <mofachhalhossain101@gmail.com>
2024-05-03 00:35:32 +0600

    Post Query Loop

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
                         Services
                     </section>
                     <section class="home-blog">
-                        Blog
+                    <?php
+                    wp_reset_postdata();
+                    if( have_posts() ):
+                        // query_posts(array('post_type' => 'post', 'posts_per_page' => 5));
+                        while( have_posts() ) : the_post(); ?>
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <div class="meta-info">
+                                    <p>Post Date: <?php echo get_the_date(); ?></p>
+                                    <p>Post By: <?php the_author_posts_link(); ?></p>
+                                    <p>Category: <?php the_category( ' ' ); ?></p>
+                                    <p>Tags: <?php the_tags( ' ' ); ?></p>
+                                </div>
+                                <?php the_content(); ?>
+                            </article>
+                        <?php endwhile;
+                    else: ?>
+                        <p>Nothing to show</p> 
+                    <?php endif; ?> 
                     </section>
                 </main>
             </div>
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,25 +1,33 @@
 // Select all parent menu items
-const parentMenuItems = document.querySelectorAll('nav ul li');
+const parentMenuItems = document.querySelectorAll("nav ul li");
+const mobileDropDown = document.querySelector("button.check-button");
 
+parentMenuItems.forEach((parentMenuItem) => {
+  parentMenuItem.addEventListener("mouseenter", () => {
+    const subMenu = parentMenuItem.querySelector("ul");
 
-parentMenuItems.forEach(parentMenuItem => {
-    parentMenuItem.addEventListener('mouseenter', () => {
-        const subMenu = parentMenuItem.querySelector('ul');
+    if (subMenu) {
+      subMenu.style.display = "block";
+      subMenu.style.position = "absolute";
+      subMenu.style.padding = "20px 10px";
+      subMenu.style.backgroundColor = "#A9A9A9";
+    }
+  });
 
-        if (subMenu) {
-            subMenu.style.display = 'block';
-            subMenu.style.position = 'absolute';
-            subMenu.style.padding = '20px 10px';
-            subMenu.style.backgroundColor = '#A9A9A9';
-        }
-    });
+  parentMenuItem.addEventListener("mouseleave", () => {
+    const subMenu = parentMenuItem.querySelector("ul");
 
+    if (subMenu) {
+      subMenu.style.display = "none";
+    }
+  });
+});
 
-    parentMenuItem.addEventListener('mouseleave', () => {
-        const subMenu = parentMenuItem.querySelector('ul');
-        
-        if (subMenu) {
-            subMenu.style.display = 'none';
-        }
-    });
+mobileDropDown.addEventListener("click", () => {
+  const mobileMenu = document.querySelector(".main-menu ul, .main-menu ul.sub-menu");
+  if (mobileMenu) {
+    mobileMenu.classList.toggle("mobile-nav-active");
+  } else {
+    console.error("Menu element not found.");
+  }
 });
